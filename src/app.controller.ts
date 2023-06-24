813ab9191e9bd34f9ed8759aa27e2200ee2dfa8a
trickaugusto <trickaugusto1206@gmail.com>
2023-06-24 14:49:32 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+
+import { Body, Controller, Get, HttpStatus, Param, Post, Redirect, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { CreateLinkQr } from './dto/CreateLinkQr';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('redirect')
+  @Redirect('', 302)
+  async redirect() {
+    return { url: 'https://www.youtube.com/watch?v=Z4-ZEHqNa-8' };
+  }
+
+  @Post('new-link')
+  async newLink(@Body() createLinkQr: CreateLinkQr) {
+    return `this action post a new link: ${createLinkQr.link}`
+  }
+}
---
+++ src/dto/CreateLinkQr.ts
@@ -0,0 +1,3 @@
+export class CreateLinkQr {
+    link: string
+}
